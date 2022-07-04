ad4a34e2f0d282d0f3bade6da722bd17e9afae3f
Anton Evdokimov <aevdokimov@pulsepoint.com>
2022-07-04 05:21:26 +0300

    Count used resources

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
 func main() {
+	totalUsedCpu, totalUsedMemory := getUsedResources("")
+	fmt.Printf("\nMilliCpuSum: %+v\nMemSum: %+v\n", totalUsedCpu, totalUsedMemory)
 
+}
+
+// Count amount of used Cpu and Memory for specified namespace and deployment prefix and suffix
+func getUsedResources(namespace string) (int64, int64) {
+	clientset := getMetricsClientset()
+	podMetricsList, err := clientset.MetricsV1beta1().PodMetricses(namespace).List(context.TODO(), metav1.ListOptions{})
+	checkErr(err)
+
+	var cpuSum, memSum int64
+
+	for _, pod := range podMetricsList.Items {
+		for _, container := range pod.Containers {
+			cpuSum += container.Usage.Cpu().MilliValue()
+			memSum += container.Usage.Memory().Value()
+		}
+	}
+
+	return cpuSum, memSum
 }
 
 func getMetaV1Clientset(apiVersion ...string) *kubernetes.Clientset {
