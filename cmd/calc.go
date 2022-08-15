41e065e49826b129a77200941a02bf42ea5c220d
Anton Evdokimov <aevdokimov@pulsepoint.com>
2022-08-15 22:09:33 +0300

    Move adjustRPS() to calc.go

--- cmd/calc.go
+++ cmd/calc.go
@@ -1,1 +1,23 @@
 package main
+
+import "math"
+
+// Apply frontend_successful_percentage and frontend_to_shared_percentage multipliers from config.yaml
+func adjustRPS(config *configType, targetNamespace string, rawRPS float64) float64 {
+	multiplier := 1.0
+
+	for _, currentNamespace := range config.Namespaces {
+		if currentNamespace.Name == targetNamespace {
+			if currentNamespace.Frontend && currentNamespace.FrontendSuccessfulPercentage != 0 {
+				multiplier *= currentNamespace.FrontendSuccessfulPercentage / 100
+			}
+
+			if currentNamespace.Frontend && currentNamespace.Shared && currentNamespace.FrontendToSharedPercentage != 0 {
+				multiplier *= currentNamespace.FrontendToSharedPercentage / 100
+			}
+		}
+	}
+
+	printDebug("Multiplier: %+v\n", multiplier)
+	return math.Round(rawRPS * multiplier)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"math"
 	"time"
 
 	promapi "github.com/prometheus/client_golang/api"
@@ -122,26 +121,6 @@ func main() {
 		printDebug("\n")
 	}
 
-}
-
-// Apply frontend_successful_percentage and frontend_to_shared_percentage multipliers from config.yaml
-func adjustRPS(config *configType, targetNamespace string, rawRPS float64) float64 {
-	multiplier := 1.0
-
-	for _, currentNamespace := range config.Namespaces {
-		if currentNamespace.Name == targetNamespace {
-			if currentNamespace.Frontend && currentNamespace.FrontendSuccessfulPercentage != 0 {
-				multiplier *= currentNamespace.FrontendSuccessfulPercentage / 100
-			}
-
-			if currentNamespace.Frontend && currentNamespace.Shared && currentNamespace.FrontendToSharedPercentage != 0 {
-				multiplier *= currentNamespace.FrontendToSharedPercentage / 100
-			}
-		}
-	}
-
-	printDebug("Multiplier: %+v\n", multiplier)
-	return math.Round(rawRPS * multiplier)
 }
 
 // Get Requests Per Second for the specified namespace (from Prometheus)
